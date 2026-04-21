37b4415a8fdfb2c837667e2cab53b002fa810d21
Mark John Pagarigan <ic.markjohnpaul.pagarigan@cvsu.edu.ph>
2026-04-21 23:27:37 +0800

    Refactor training pipeline and mobile inference flow

---
+++ MOBILE_APP/ios/build/Pods.build/Release-iphoneos/permission_handler_apple.build/Objects-normal/arm64/CriticalAlertsPermissionStrategy.d
@@ -0,0 +1,8 @@
+dependencies: \
+  /Users/mj/Documents/GitHub/TomoLeafNet-v1/MOBILE_APP/ios/build/Pods.build/Release-iphoneos/permission_handler_apple.build/module.modulemap \
+  /Users/mj/.pub-cache/hosted/pub.dev/permission_handler_apple-9.4.7/ios/Classes/strategies/CriticalAlertsPermissionStrategy.m \
+  /Users/mj/Documents/GitHub/TomoLeafNet-v1/MOBILE_APP/ios/Pods/Target\ Support\ Files/permission_handler_apple/permission_handler_apple-prefix.pch \
+  /Users/mj/.pub-cache/hosted/pub.dev/permission_handler_apple-9.4.7/ios/Classes/strategies/CriticalAlertsPermissionStrategy.h \
+  /Users/mj/.pub-cache/hosted/pub.dev/permission_handler_apple-9.4.7/ios/Classes/strategies/PermissionStrategy.h \
+  /Users/mj/.pub-cache/hosted/pub.dev/permission_handler_apple-9.4.7/ios/Classes/PermissionHandlerEnums.h \
+  /Users/mj/.pub-cache/hosted/pub.dev/permission_handler_apple-9.4.7/ios/Classes/strategies/UnknownPermissionStrategy.h
